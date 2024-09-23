454c5f4275799e5af4ee1acff9d270916171df43
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-09-23 17:13:41 -0400

    initial commit

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,14 +1,43 @@
-resource "aws_iam_user_group_membership" "django" {
-  user   = aws_iam_user.django.name
-  groups = ["Django-Static+Media-S3"]
-}
-
 resource "aws_iam_user" "django" {
   name = var.project_name
   path = "/users/"
 
   tags = {
     Project = var.project_name
+  }
+}
+
+resource "aws_iam_user_policy" "django" {
+  name   = "django-storages-permission-${var.project_name}"
+  user   = aws_iam_user.django.name
+  policy = data.aws_iam_policy_document.django_user.json
+}
+
+data "aws_iam_policy_document" "django_user" {
+  statement {
+    sid    = "1"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions"
+    ]
+    resources = [
+      aws_s3_bucket.main.arn
+    ]
+  }
+  statement {
+    sid    = "2"
+    effect = "Allow"
+    actions = [
+      "s3:*Object*",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload"
+    ]
+    resources = [
+      "${aws_s3_bucket.main.arn}/*"
+    ]
   }
 }
 
